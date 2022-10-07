600c403e493e41e3da98b7c419d9ba6b1ceae472
Nice <puma1800@naver.com>
2022-10-08 01:41:55 +0900

    테일윈드 작업

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App(){
 
   const [todoData, setTodoData]= useState([]);
   const [value, setValue] = useState("");
-  
+
   const handleSubmit = (e) => {
     e.preventDefault();
     let newTodo = {
@@ -20,9 +20,9 @@ export default function App(){
   };
   
     return (
-      <div className="container">
-        <div className="todoBlock">
-          <div className="title">
+      <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
+        <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg">
+          <div className="flex justify-between mb-3">
             <h1>할 일 목록</h1>
             </div>
             <Form value={value} setValue={setValue} handleSubmit={handleSubmit}/>
--- src/components/List.js
+++ src/components/List.js
@@ -2,47 +2,36 @@ import React from 'react'
 
 export default function List({ todoData, setTodoData }) {
 
-    const btnStyle = {
-        color: "#fff",
-        border: "none",
-        padding: "5px 9px",
-        borderRadius: "50%",
-        cursor: "pointer",
-        float: "right"
-    };
-
-    const getStyle = (completed) => {
-        return {
-            padding: "10px",
-            borderBottom: "1px #ccc dotted",
-            textDecoration: completed ? 'line-through' : 'none'
-        }
-    };
-
     const handleClick = (id) => {
         let newTodoData = todoData.filter((data) => (data.id !== id));
         setTodoData(newTodoData);
     };
 
-    const handCompleChange = (id) => {
-        let newTodoData = todoData.map((data) => {
-            if (data.id === id) {
-                data.completed = !data.completed;
-            }
-            return data;
-        });
-        setTodoData(newTodoData);
-    };
+    const handCompleChange = (id) => {let newTodoData = todoData.map((data)=>{ 
+        if(data.id===id){
+            data.completed = !data.completed;
+        } 
+        return data;
+    });
+        setTodoData(newTodoData);};
 
     return (
-            todoData.map((data) => (
-                <div style={getStyle(data.completed)} key={data.id}>
-                    <input type="checkbox"
+        <div>
+            {todoData.map((data) => (
+                <div key={data.id}>
+                    <div className='flex items-center justify-between w-full px-4 py-1 text-gray-600 bg-gray-100 border rounded'>
+                    <div className='items-center'>
+                    <input type='checkbox'
                         onChange={() => handCompleChange(data.id)}
-                        defaultChecked={false} />
-                    {data.title}
-                    <button style={btnStyle} onClick={() => handleClick(data.id)}>x</button>
+                        defaultChecked={data.completed} />{" "}
+                    <span className={data.completed ? 'line-through': undefined}>{data.title}</span>
+                    </div>
+                    <div className='items-center'>
+                    <button  className='px-4 py-2 float-right' onClick={() => handleClick(data.id)}>x</button>
+                    </div>
                 </div>
-            ))
+                </div>
+            ))}
+        </div>
     )
 };
