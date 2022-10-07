0be5dec515635efb95b0b98512972f0319f450cb
Nice <puma1800@naver.com>
2022-10-07 17:12:20 +0900

    리스트 컴포넌트 분리

---
+++ src/components/List.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+
+export default function List({ todoData, setTodoData }) {
+
+    const btnStyle = {
+        color: "#fff",
+        border: "none",
+        padding: "5px 9px",
+        borderRadius: "50%",
+        cursor: "pointer",
+        float: "right"
+    };
+
+    const getStyle = (completed) => {
+        return {
+            padding: "10px",
+            borderBottom: "1px #ccc dotted",
+            textDecoration: completed ? 'line-through' : 'none'
+        }
+    };
+
+    const handleClick = (id) => {
+        let newTodoData = todoData.filter((data) => (data.id !== id));
+        setTodoData(newTodoData);
+    };
+
+    const handCompleChange = (id) => {
+        let newTodoData = todoData.map((data) => {
+            if (data.id === id) {
+                data.completed = !data.completed;
+            }
+            return data;
+        });
+        setTodoData(newTodoData);
+    };
+
+    return (
+        {
+            todoData.map((data) => (
+            <div style={getStyle(data.completed)} key={data.id}>
+                <input type="checkbox"
+                    onChange={() => handCompleChange(data.id)}
+                    defaultChecked={false} />
+                {data.title}
+                <button style={btnStyle} onClick={() => handleClick(data.id)}>x</button>
+            </div>
+            ))
+        }
+    )
+}
