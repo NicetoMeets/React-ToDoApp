a946653cca638f03dc554be56aa8589e97a0b192
Nice <puma1800@naver.com>
2022-10-08 14:45:11 +0900

    할일 수정 저장기능

--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import { data } from 'autoprefixer';
+import React, { useState } from 'react'
 
 const List = React.memo(({
     id, title, completed, todoData, setTodoData, provided, snapshot,
 }) => {
-    
+
+    const [isEditing, setIsEditing] = useState(false);
+    const [editedTitle, setEditedTitle] = useState(title);
+
     const handleClick = (id) => {
         let newTodoData = todoData.filter((data) => (data.id !== id));
         setTodoData(newTodoData);
@@ -19,6 +23,45 @@ const List = React.memo(({
         setTodoData(newTodoData);
     };
 
+    const handleEditChange = (e) => {
+        setEditedTitle(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        let newTodoData = todoData.map((data)=>{
+            if(data.id === id){
+                data.title = editedTitle
+            }
+            return data;
+        });
+        setTodoData(newTodoData);
+        setIsEditing(false)
+    };
+
+
+    if (isEditing) {
+        return (
+            <div className={
+                    'flex items-center justify-between w-full px-4 py-1 text-gray-600 bg-gray-100 border rounded'}>
+                <div className='items-center'>
+                    <form onSubmit={handleSubmit}>
+                        <input
+                        value={editedTitle}
+                        className='w-full px-3 py-2 mr-4 text-gray-500 rounded'
+                        onChange={handleEditChange}
+                        />
+                    </form>
+                </div>
+                <div className='items-center'>
+                    <button className='px-4 py-2 float-right' onClick={() => setIsEditing(false)}>x</button>
+                    <button className='px-4 py-2 float-right' onClick={handleSubmit} type='submit'>save</button>
+                </div>
+            </div >
+        )
+    } else {
+
     return (
         <div key={id} {...provided.draggableProps} ref={provided.innerRef} {...provided.dragHandleProps}
             className={`${snapshot.isDragging ? 'bg-gray-400' : 'bg-gray-100'} 
@@ -31,9 +74,11 @@ const List = React.memo(({
             </div>
             <div className='items-center'>
                 <button className='px-4 py-2 float-right' onClick={() => handleClick(id)}>x</button>
+                <button className='px-4 py-2 float-right' onClick={() => setIsEditing(true)}>edit</button>
             </div>
         </div>
-    )
+    );
+}
 });
 
 export default List;
