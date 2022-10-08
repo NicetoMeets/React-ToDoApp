6b0d673fca8235743e205f7f36a1f369bf838bce
Nice <puma1800@naver.com>
2022-10-08 13:34:59 +0900

    React memo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import "./App.css";
-import List from "./components/List";
+import Lists from "./components/Lists";
 import Form from "./components/Form";
 
-export default function App(){
+export default function App() {
 
-  const [todoData, setTodoData]= useState([]);
+  const [todoData, setTodoData] = useState([]);
   const [value, setValue] = useState("");
 
   const handleSubmit = (e) => {
@@ -15,19 +15,19 @@ export default function App(){
       title: value,
       completed: false,
     };
-    setTodoData(prev=>[...prev, newTodo]);
+    setTodoData(prev => [...prev, newTodo]);
     setValue("");
   };
-  
-    return (
-      <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
-        <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg">
-          <div className="flex justify-between mb-3">
-            <h1>할 일 목록</h1>
-            </div>
-            <Form value={value} setValue={setValue} handleSubmit={handleSubmit}/>
-            <List todoData={todoData} setTodoData={setTodoData}/>
+
+  return (
+    <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
+      <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg">
+        <div className="flex justify-between mb-3">
+          <h1>할 일 목록</h1>
         </div>
+        <Form value={value} setValue={setValue} handleSubmit={handleSubmit} />
+        <Lists todoData={todoData} setTodoData={setTodoData} />
       </div>
-    )
-  };
+    </div>
+  )
+};
--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 
-
-export default function List({ todoData, setTodoData }) {
-
+const List = ({
+    key, id, title, completed, todoData, setTodoData, provided, snapshot
+}) => {
     const handleClick = (id) => {
         let newTodoData = todoData.filter((data) => (data.id !== id));
         setTodoData(newTodoData);
@@ -19,58 +18,21 @@ export default function List({ todoData, setTodoData }) {
         setTodoData(newTodoData);
     };
 
-    const handleEnd = (result) => {
-        // result 매개변수에는 source 항목 및 대상 위치와 같은 드래그 이벤트에 대한 정보가 포함됩니다.
-
-        // 목적지가 없으면(이벤트 취소) 이 함수를 종료합니다.
-        if(!result.destination) return;
-
-        //리액트 불변성을 지켜주기 위해 새로운 todoData 생성
-        const newTodoData = todoData;
-
-        //1. 변경시키는 아이템을 배열에서 지워줍니다.
-        //2. return 값으로 지워진 아이템을 잡아줍니다.
-        const [reorderedItem] = newTodoData.splice(result.source.index, 1);
-        
-        //원하는 자리에 reorderedItem을 insert 해줍니다.
-        newTodoData.splice(result.destination.index, 0 , reorderedItem);
-        setTodoData(newTodoData);
-    }
-
     return (
-        <div>
-            <DragDropContext onDragEnd={handleEnd}>
-                <Droppable droppableId='todo'>
-                    {(provided) => (
-                        <div {...provided.droppableProps} ref={provided.innerRef}>
-                            {todoData.map((data, index) => (
-                                <Draggable
-                                    key={data.id}
-                                    draggableId={data.id.toString()}
-                                    index={index}
-                                >
-                                    {(provided, snapshot) => (
-                                        <div key={data.id} {...provided.draggableProps} ref={provided.innerRef} {...provided.dragHandleProps}
-                                            className={`${snapshot.isDragging ? 'bg-gray-400' : 'bg-gray-100'} 
+        <div key={id} {...provided.draggableProps} ref={provided.innerRef} {...provided.dragHandleProps}
+            className={`${snapshot.isDragging ? 'bg-gray-400' : 'bg-gray-100'} 
                                             flex items-center justify-between w-full px-4 py-1 text-gray-600 bg-gray-100 border rounded`}>
-                                            <div className='items-center'>
-                                                <input type='checkbox'
-                                                    onChange={() => handCompleChange(data.id)}
-                                                    defaultChecked={data.completed} />{" "}
-                                                <span className={data.completed ? 'line-through' : undefined}>{data.title}</span>
-                                            </div>
-                                            <div className='items-center'>
-                                                <button className='px-4 py-2 float-right' onClick={() => handleClick(data.id)}>x</button>
-                                            </div>
-                                        </div>
-                                    )}
-                                </Draggable>
-                            ))}
-                            {provided.placeholder}
-                        </div>
-                    )}
-                </Droppable>
-            </DragDropContext>
+            <div className='items-center'>
+                <input type='checkbox'
+                    onChange={() => handCompleChange(id)}
+                    defaultChecked={completed} />{" "}
+                <span className={completed ? 'line-through' : undefined}>{title}</span>
+            </div>
+            <div className='items-center'>
+                <button className='px-4 py-2 float-right' onClick={() => handleClick(id)}>x</button>
+            </div>
         </div>
     )
-};
+}
+
+export default List
